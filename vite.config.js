f722dfc43c6b591e6dc8f9136184857fe344f4b1
RachelChen <iceage91@gmail.com>
2022-07-27 00:31:29 +0800

    Removed unused codes

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'url'
 import typescript2 from "rollup-plugin-typescript2";
-// https://vitejs.dev/config/
-// export default ({ mode }) => {
-//   const env = loadEnv(mode, process.cwd())
-//   return defineConfig({
-//     define: {
-//       'process.env': { ...env }
-//     },
-//     plugins: [vue()]
-//   })
-// }
 
 export default ({ mode }) => {
   const env = loadEnv(mode, process.cwd())
